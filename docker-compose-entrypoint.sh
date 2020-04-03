3b9c842bca816219a465e7d7e14e3f7748f77ded
Karl Wettin <karl.wettin@wikimedia.se>
2020-04-03 18:26:03 +0200

    Wait for other services to start when using docker-compose up

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Waiting for all other services to start..."
+
+./wait-for-it.sh -h mishkal -p 8080 -t 60
+./wait-for-it.sh -h mary-tts -p 59125 -t 60
+./wait-for-it.sh -h pronlex -p 8787 -t 60
+
 echo "Starting Wikispeech mockup."
 cd src/wikispeech_mockup
 python3 bin/wikispeech docker-compose.conf
