08125d8577ac834c9c8be675079e7afe99a08c6c
Karl Wettin <karl.wettin@wikimedia.se>
2020-04-15 15:32:31 +0200

    waitfor symbolset

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -5,6 +5,7 @@ echo "Waiting for all other services to start..."
 ./wait-for-it.sh -h mishkal -p 8080 -t 60
 ./wait-for-it.sh -h mary-tts -p 59125 -t 60
 ./wait-for-it.sh -h pronlex -p 8787 -t 60
+./wait-for-it.sh -h symbolset -p 8771 -t 60
 
 echo "Starting Wikispeech mockup."
 cd src/wikispeech_mockup
